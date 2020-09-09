f38b9fe9d46d4f590d2d7071fec2f92c9ec55299
Mat Allen <mallen@redhat.com>
2020-09-09 17:13:51 -0500

    debugging auth for new domain name

--- server/src/main/java/com/redhat/services/ae/controllers/AuthenticationController.java
+++ server/src/main/java/com/redhat/services/ae/controllers/AuthenticationController.java
@@ -111,7 +111,7 @@ public class AuthenticationController{
 //						.header("Access-Control-Allow-Methods", "GET, POST")
 //						.header("Access-Control-Allow-Headers", "Content-Type, *")
 						.cookie(new NewCookie("rhae-jwt", jwtToken, "/", domainName, "__SAME_SITE_NONE__", 60*60 /*1hr*/, false, false))
-						.header("Set-Cookie", "key=value; path=/; domain=.redhat.com; SameSite=none;")
+						.header("Set-Cookie", "key=value;Path=/;Domain=.redhat.com;Max-Age="+(60*60)+"; SameSite=none;")
 						.build();
 				
 			}else{
