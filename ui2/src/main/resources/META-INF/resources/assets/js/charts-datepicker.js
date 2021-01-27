d3fe7294c69a6c2edddae26b234a416b0ec3b6f7
Mat Allen <mallen@redhat.com>
2021-01-27 17:34:11 -0600

    fixed defects. 1-CY20 in date picker didnt move over to CY21, 2-Current month not appearing in graphs, 3-answer distribution graph not updating on filter change

--- ui2/src/main/resources/META-INF/resources/assets/js/charts-datepicker.js
+++ ui2/src/main/resources/META-INF/resources/assets/js/charts-datepicker.js
@@ -10,7 +10,7 @@ function dateToYYYYMMM(d){
 	return d.getFullYear()+"-"+monthNames[d.getMonth()].slice(0,3);
 }
 function getCYYear(d){
-	return (d.getFullYear()+"").slice(0,2);
+	return (d.getFullYear()+"").slice(2,4);
 }
 function getCYQ(d){
 	return "Q"+Math.ceil((d.getMonth()+1)/3); // ie. 1-3=1, 4-6=2, 7-9=3 & 10-12=4
@@ -31,9 +31,14 @@ var dateThisQEnd=dateMinusMonths(currentMonth, -1*monthNumerialToEndOfQForCY(now
 function generateDateRangeSelect(name, selected){
 	var currentMonthYYYYMMM=dateToYYYYMMM(currentMonth);
 	var lastMonthYYYYMMM=dateToYYYYMMM(lastMonth);
-	var minus3months=dateToYYYYMMM(dateMinusMonths(lastMonth,2));
-	var minus6months=dateToYYYYMMM(dateMinusMonths(lastMonth,5));
-	var minus9months=dateToYYYYMMM(dateMinusMonths(lastMonth,8));
+	
+	var endMonth=currentMonth;
+	var endMonthYYYYMMM=currentMonthYYYYMMM;
+	
+	var targetEndMonthYYYYMMM=dateToYYYYMMM(endMonth);
+	var minus3months=dateToYYYYMMM(dateMinusMonths(endMonth,2));
+	var minus6months=dateToYYYYMMM(dateMinusMonths(endMonth,5));
+	var minus9months=dateToYYYYMMM(dateMinusMonths(endMonth,8));
 	var thisQuarterStart=dateToYYYYMMM(dateThisQStart);
 	var thisQuarterEnd=dateToYYYYMMM(dateThisQEnd);
 	var thisQuarterLabel="CY"+getCYYear(dateThisQStart)+" "+getCYQ(dateThisQStart)+" ("+thisQuarterStart+" -> "+thisQuarterEnd+")";
@@ -45,11 +50,11 @@ function generateDateRangeSelect(name, selected){
 	var result="<select id='"+name+"Months' style='color: #333' onchange='"+name+"refresh(); return false;'>";
 	result+="<option "+(selected==thisQuarterLabel?"selected":"")+" value='start="+thisQuarterStart+"&end="+thisQuarterEnd+"' data-start='"+thisQuarterStart+"' data-end='"+thisQuarterEnd+"'>"+thisQuarterLabel+"</option>";
 	result+="<option "+(selected==lastQuarterLabel?"selected":"")+" value='start="+lastQuarterStart+"&end="+lastQuarterEnd+"' data-start='"+lastQuarterStart+"' data-end='"+lastQuarterEnd+"'>"+lastQuarterLabel+"</option>";
-//	result+="<option "+(selected=="Current Month"?"selected":"")+" value='start="+currentMonthYYYYMMM+"&end="+currentMonthYYYYMMM+"' data-start='"+currentMonthYYYYMMM+"' data-end='"+currentMonthYYYYMMM+"'>Current Month</option>";
+	result+="<option "+(selected=="Current Month"?"selected":"")+" value='start="+currentMonthYYYYMMM+"&end="+currentMonthYYYYMMM+"' data-start='"+currentMonthYYYYMMM+"' data-end='"+currentMonthYYYYMMM+"'>Current Month</option>";
 	result+="<option "+(selected=="Last Month"?"selected":"")+" value='start="+lastMonthYYYYMMM+"&end="+lastMonthYYYYMMM+"' data-start='"+lastMonthYYYYMMM+"' data-end='"+lastMonthYYYYMMM+"'>Last Month</option>";
-	result+="<option "+(selected=="3 Months"?"selected":"")+"   value='start="+minus3months+"&end="+lastMonthYYYYMMM+"'     data-start='"+minus3months+"'     data-end='"+lastMonthYYYYMMM+"'>3 Months</option>";
-	result+="<option "+(selected=="6 Months"?"selected":"")+"   value='start="+minus6months+"&end="+lastMonthYYYYMMM+"'     data-start='"+minus6months+"'     data-end='"+lastMonthYYYYMMM+"'>6 Months</option>";
-	result+="<option "+(selected=="9 Months"?"selected":"")+"   value='start="+minus9months+"&end="+lastMonthYYYYMMM+"'     data-start='"+minus9months+"'     data-end='"+lastMonthYYYYMMM+"'>9 Months</option>";
+	result+="<option "+(selected=="3 Months"?"selected":"")+"   value='start="+minus3months+"&end="+    endMonthYYYYMMM+"' data-start='"+minus3months+"'     data-end='"+endMonthYYYYMMM+"'>3 Months</option>";
+	result+="<option "+(selected=="6 Months"?"selected":"")+"   value='start="+minus6months+"&end="+    endMonthYYYYMMM+"' data-start='"+minus6months+"'     data-end='"+endMonthYYYYMMM+"'>6 Months</option>";
+	result+="<option "+(selected=="9 Months"?"selected":"")+"   value='start="+minus9months+"&end="+    endMonthYYYYMMM+"' data-start='"+minus9months+"'     data-end='"+endMonthYYYYMMM+"'>9 Months</option>";
 //	result+="<option "+(selected=="9 Months (incl Current)"?"selected":"")+"   value='start="+minus9months+"&end="+currentMonthYYYYMMM+"'     data-start='"+minus9months+"'     data-end='"+currentMonthYYYYMMM+"'>9 Months (incl Current)</option>";
 	return result+"</select>";
 }
